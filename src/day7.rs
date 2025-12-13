cc5a619f0d8f1f3df4d9ca8e5899d53ffb70bfc9
David Wheeler <daveofthings@gmail.com>
2025-12-12 23:08:29 -0500

    Day 7, part 1 complete.

--- src/day7.rs
+++ src/day7.rs
@@ -1,17 +1,47 @@
+use std::collections::HashSet;
+
 use crate::day::{Day, Answer};
 
 // A representation of the puzzle inputs.
 struct Input {
+    rows: Vec<Vec<char>>,
 }
 
 impl Input {
     fn read(text: &str) -> Input 
     {
-        for _line in text.lines() {
-            // Process one line
+        let mut rows = Vec::new();
+        for line in text.lines() {
+            let mut row = Vec::new();
+            for c in line.chars() {
+                row.push(c);
+            }
+            rows.push(row);
         }
 
-        Input { }
+        Input { rows }
+    }
+
+    fn splits(&self) -> usize {
+        let mut beams: HashSet<usize> = HashSet::new();
+        let mut splits = 0;
+
+        for row in &self.rows {
+            for (n, c) in row.iter().enumerate() {
+                if *c == 'S' {
+                    beams.insert(n);
+                }
+                if *c == '^' && beams.contains(&n) {
+                    // split the beam
+                    splits += 1;
+                    beams.remove(&n);
+                    beams.insert(n-1);
+                    beams.insert(n+1);
+                }
+            }
+        }
+
+        splits
     }
 }
 
@@ -29,9 +59,9 @@ impl Day for Day7 {
     // Compute Part 1 solution
     fn part1(&self, text: &str) -> Answer {
         // Read input file into Input struct
-        let _input = Input::read(text);
+        let input = Input::read(text);
 
-        Answer::None
+        Answer::Numeric(input.splits())
     }
 
     fn part2(&self, text: &str) -> Answer {
@@ -49,13 +79,41 @@ mod test {
     use crate::day::{Day, Answer};
     
     const EXAMPLE1: &str = "\
-Example input
+.......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............
 ";
 
     #[test]
     // Read part 1 example and confirm inputs
     fn test_read_part1() {
-        let _input = Input::read(EXAMPLE1);
+        let input = Input::read(EXAMPLE1);
+
+        assert_eq!(input.rows.len(), 16);
+        assert_eq!(input.rows[0].len(), 15);
+        assert_eq!(input.rows[0][0], '.');
+        assert_eq!(input.rows[0][7], 'S');
+        assert_eq!(input.rows[2][7], '^');
+    }
+
+    #[test]
+    fn test_splits() {
+        let input = Input::read(EXAMPLE1);
+
+        assert_eq!(input.splits(), 21);
     }
 
     #[test]
@@ -63,7 +121,7 @@ Example input
     fn test_part1() {
         // Based on the example in part 1.
         let d = Day7::new();
-        assert_eq!(d.part1(EXAMPLE1), Answer::None);
+        assert_eq!(d.part1(EXAMPLE1), Answer::Numeric(21));
     }
 
 
